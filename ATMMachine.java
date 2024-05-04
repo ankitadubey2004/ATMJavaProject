56897f4e03a72c1ab43a4f2de5897ceede7e5e51
ankitadubey2004 <155213532+ankitadubey2004@users.noreply.github.com>
2024-05-04 20:13:18 +0530

    added

--- ATMMachine.java
+++ ATMMachine.java
@@ -15,7 +15,7 @@ class ATM {
         }
     }
 
-    public void menu(){
+    public void menu() {
         System.out.println("Enter Your Choice: ");
         System.out.println("1. Check A/C Balance");
         System.out.println("2. Withdraw Money");
@@ -25,21 +25,51 @@ class ATM {
         Scanner sc = new Scanner(System.in);
         int opt = sc.nextInt();
 
-
-        if(opt == 1){
+        if (opt == 1) {
             checkBalance();
-        }
-        else if(opt == 2){
+        } else if (opt == 2) {
             withdrawMoney();
-        }
-        else if(opt == 3 ){
+        } else if (opt == 3) {
             depositMoney();
+        } else if (opt == 4) {
+            return;
+        } else {
+            System.out.println("Enter a valid choice");
         }
-        else if(opt == 4){
-            break;
+    }
+
+    public void checkBalance() {
+        System.out.println("Balance: " + Balance);
+        menu();
+    }
+
+    public void withdrawMoney() {
+        System.out.println("Enter Amount to Withdraw");
+        Scanner sc = new Scanner(System.in);
+        float amount = sc.nextFloat();
+        if (amount > Balance) {
+            System.out.println("Insufficient Balance");
+        } else {
+            Balance = Balance - amount;
+            System.out.println("Money Withdrawl Succesful");
         }
-        else{
-            System.out.println('Enter a valid choice');
-        }
+        menu();
+    }
+
+    public void depositMoney() {
+        System.out.println("Enter the amount: ");
+        Scanner sc = new Scanner(System.in);
+        float amount = sc.nextFloat();
+        Balance = Balance + amount;
+        System.out.println("Money deposited Succesfully");
+        menu();
+    }
+}
+
+public class ATMMachine {
+    public static void main(String[] args) {
+
+        ATM obj = new ATM();
+        obj.checkpin();
     }
 }
